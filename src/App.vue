f205375f8e1f1b1becc7e2ede67806d0ab840c3b
SuperSonic <supersonic@livemail.tw>
2021-08-21 14:24:03 +0800

    Improve UI

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         :width="$store.state.screenSize.width"
         class="mx-auto overflow-hidden"
     >
-      <v-app-bar app extended fixed flat>
+      <v-app-bar :extended="flexible[0]" app fixed flat>
         <v-app-bar-title>
           <v-img
               :src="toolbarLogoUrl"
@@ -56,7 +56,7 @@
         </v-list>
         <template #append>
           <div class="pa-2">
-            <v-btn block class="primary" rounded link to="/raise">
+            <v-btn block class="primary" link rounded to="/raise">
               {{ $t("nav.raise") }}
             </v-btn>
           </div>
@@ -64,9 +64,10 @@
       </v-navigation-drawer>
       <v-main>
         <v-card
-            :height="$store.state.screenSize.height - 105"
+            :height="$store.state.screenSize.height - flexible[1]"
             :width="$store.state.screenSize.width"
             class="mx-auto overflow-y-auto"
+            @scroll="mainScrolled"
             flat
         >
           <router-view/>
@@ -81,9 +82,12 @@ export default {
   name: 'App',
   data: () => ({
     drawer: false,
-    status: false,
+    scrolled: false,
   }),
   computed: {
+    flexible() {
+      return this.scrolled ? [false, 60] : [true, 105];
+    },
     toolbarLogoUrl() {
       const logoUrl = process.env.VUE_APP_LOGO_URL;
       return logoUrl || "default.png";
@@ -100,6 +104,9 @@ export default {
         height: window.innerHeight
       })
     },
+    mainScrolled(e) {
+      this.scrolled = e.target.scrollTop > 100
+    }
   },
   async created() {
     this.resize()
