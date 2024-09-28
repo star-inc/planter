8bdd771202f2219f993c5efcee40ff4852e8b7df
不知火 Shiranui <supersonic@livemail.tw>
2024-09-28 20:29:28 +0800

    feat: add index into worker

--- src/workers/index.js
+++ src/workers/index.js
@@ -1,10 +1,22 @@
-import { AutoRouter } from 'itty-router';
+import {
+  AutoRouter,
+  cors,
+} from "itty-router";
 
-const router = AutoRouter();
+const { preflight, corsify } = cors();
+const router = AutoRouter({
+  before: [preflight],
+  finally: [corsify],
+});
 
-router
-  .get('/states/:id', ({id}) => {
-	return id;
+router.
+  get("/", (_, env) => {
+    const dstUrl = env.PAGE_ORIGIN;
+    const statusCode = 301;
+    return Response.redirect(dstUrl, statusCode);
+  }).
+  get("/states/:id", ({ id }) => {
+    return id;
   });
 
 export default router;
